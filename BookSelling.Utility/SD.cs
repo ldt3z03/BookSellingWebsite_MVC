8bbf6a4bfb5e29b5fb5fd7670b7a43baed47cdaf
ldt3z03 <kaitokun206203@gmail.com>
2024-04-24 20:48:33 +0700

    Add session to shopping cart

--- BookSelling.Utility/SD.cs
+++ BookSelling.Utility/SD.cs
@@ -25,5 +25,7 @@ namespace BookSelling.Utility
 		public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
 		public const string PaymentStatusRejected = "Rejected";
 
+        public const string SessionCart = "SessionShoppingCart";
+
 	}
 }
--- BookSellingWebsite/Areas/Customer/Controllers/HomeController.cs
+++ BookSellingWebsite/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 
 using BookSelling.DataAccess.Repository.IRepository;
 using BookSelling.Models;
+using BookSelling.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -24,6 +25,15 @@ namespace BookSellingWebsite.Areas.Customer.Controllers
 
         public IActionResult Index()
         {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if(claim != null)
+            {
+                HttpContext.Session.SetInt32(SD.SessionCart,
+                    _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).Count());
+            }
+
             IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category");
             return View(productList);
         }
@@ -53,15 +63,19 @@ namespace BookSellingWebsite.Areas.Customer.Controllers
                 //shopping cart exists
                 cartFromDb.Count += shoppingCart.Count;
                 _unitOfWork.ShoppingCart.Update(cartFromDb);
+                _unitOfWork.Save();
             }
             else
             {
                 //add cart record
                 _unitOfWork.ShoppingCart.Add(shoppingCart);
+                _unitOfWork.Save();
+                HttpContext.Session.SetInt32(SD.SessionCart,
+                    _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId).Count());
             }
             TempData["success"] = "Cart updated successfully";
             
-            _unitOfWork.Save();
+            
 
 
             return RedirectToAction(nameof(Index));
