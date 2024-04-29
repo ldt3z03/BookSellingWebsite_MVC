78914d137122c597da7b36d716a6370a29cad6d5
ldt3z03 <kaitokun206203@gmail.com>
2024-04-29 21:52:56 +0700

    Update UserController

--- BookSelling.DataAccess/Repository/ApplicationUserRepository.cs
+++ BookSelling.DataAccess/Repository/ApplicationUserRepository.cs
@@ -17,5 +17,9 @@ namespace BookSelling.DataAccess.Repository
         {
             _db = db;
         }
+        public void Update(ApplicationUser applicationUser)
+        {
+            _db.ApplicationUsers.Update(applicationUser);
+        }
     }
 }
