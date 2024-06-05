4295ed6bfcf188ee364a1700b10374b8b7fa6273
thang <hahuythang159@gmail.com>
2024-06-05 16:15:59 +0700

    Add ChatBox

--- BookSellingWebsite/Program.cs
+++ BookSellingWebsite/Program.cs
@@ -43,13 +43,13 @@ builder.Services.AddAuthentication().AddFacebook(options =>
     options.AppSecret = "466f2ea542f486da22867a758f6483fc";
 });
 
-builder.Services.AddAuthentication().AddGoogle(options =>
-{
-    options.ClientId = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_ID");
-    options.ClientSecret = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_SECRET");
+//builder.Services.AddAuthentication().AddGoogle(options =>
+//{
+//    options.ClientId = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_ID");
+//    options.ClientSecret = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_SECRET");
 
 
-});
+//});
 
 
 
